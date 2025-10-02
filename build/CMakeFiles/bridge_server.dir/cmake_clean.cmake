2813bac05ae3ee531023d269dc64247b90d82482
dinesh-murugan-h <dineshmurugz@gmail.com>
2025-10-02 20:07:35 +0800

    Initial commit

---
+++ build/CMakeFiles/bridge_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bridge_server.dir/src/bridge_server.c.o"
+  "CMakeFiles/bridge_server.dir/src/bridge_server.c.o.d"
+  "CMakeFiles/bridge_server.dir/src/static_model.c.o"
+  "CMakeFiles/bridge_server.dir/src/static_model.c.o.d"
+  "bridge_server"
+  "bridge_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bridge_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
