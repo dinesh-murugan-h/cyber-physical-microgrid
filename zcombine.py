51a1100c7caa29412cffcd109b3cf4d84293476e
dineshmurugz <dineshmurugz@gmail.com>
2026-03-31 03:08:24 +0800

    update

--- zcombine.py
+++ zcombine.py
@@ -18,9 +18,9 @@ IGNORE_DIRS = {
     "build",
     "libiec61850",
     "libmodbus",
-    "pcc1",
-    "gen1",
-    "gen2"
+  #  "pcc1",
+  #  "gen1",
+   # "gen2"
 }
 # ==========================
 
